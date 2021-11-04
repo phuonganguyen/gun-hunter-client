9c678cac005db1444d7b80b7b762acb24eed8a35
phuonganguyen <phuong.nguyenanh06@gmail.com>
2021-11-04 23:17:19 +0700

    build

---
+++ build/web-mobile/src/settings.75944.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/splash.fire",orientation:"",jsList:[],bundleVers:{resources:"09094",internal:"1b082",main:"d3017"}};
