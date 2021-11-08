b0229134192f544c99400cf08c859b21a646e615
phuonganguyen <phuong.nguyenanh06@gmail.com>
2021-11-08 16:46:45 +0700

    change lobby_background

--- build/web-mobile/src/settings.68eef.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/splash.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",resources:"09094",main:"04b3e"}};
